cb1da59451b0faee5d8f74d869d0e8d50fa375a5
fiiwebarry <fiiwebarry@yahoo.com>
2024-06-23 09:26:13 +0100

    update

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(function (request, response, next) {
+  console.log(request.protocol, request.url, request.method);
+  next();
+});
+
 app.get('/', function (request, response) {
   response.send('Hello');
 });
@@ -12,6 +21,15 @@ app.get('/about', function (request, response) {
 
 app.get('/contact-us', function (request, response) {
   response.send('hello my gee');
+});
+
+app.post('/login', function (request, response) {
+  console.log('request body: ', request.body);
+  console.log('Content type: ', request.headers['content-type']);
+  response.send({
+    isSuccessful: true,
+    message: 'Logged in successfully',
+  });
 });
 
 app.post(function (request, response) {});
